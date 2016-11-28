7ba14a8a69a6ef934caa20bfb7af6e11903082d9
Raissa Ferreira <rai200890@gmail.com>
2016-11-28 15:54:22 -0200

    Add map dimensions to department payload (#22)

--- app/serializers/full_department_serializer.rb
+++ app/serializers/full_department_serializer.rb
@@ -1,4 +1,4 @@
 # frozen_string_literal: true
 class FullDepartmentSerializer < ActiveModel::Serializer
-  attributes :id, :name, :campus_name, :map_url, :map_bounds, :map_center
+  attributes :id, :name, :campus_name, :map_url, :map_bounds, :map_center, :map_dimensions
 end
